32dbf59c037a0eb84723ef32aab33c6ab1481483
Michael Weterings <mail@michaelweterings.dev>
2025-04-23 22:23:36 +0200

    refactor: removes obsolete locations

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,14 @@
 import { useMemo } from 'react';
-import { locations } from '@contentful/app-sdk';
-import ConfigScreen from './locations/ConfigScreen';
+import { locations, FieldAppSDK } from '@contentful/app-sdk';
 import Field from './locations/Field';
-import EntryEditor from './locations/EntryEditor';
-import Dialog from './locations/Dialog';
-import Sidebar from './locations/Sidebar';
-import Page from './locations/Page';
-import Home from './locations/Home';
 import { useSDK } from '@contentful/react-apps-toolkit';
 
 const ComponentLocationSettings = {
-  [locations.LOCATION_APP_CONFIG]: ConfigScreen,
   [locations.LOCATION_ENTRY_FIELD]: Field,
-  [locations.LOCATION_ENTRY_EDITOR]: EntryEditor,
-  [locations.LOCATION_DIALOG]: Dialog,
-  [locations.LOCATION_ENTRY_SIDEBAR]: Sidebar,
-  [locations.LOCATION_PAGE]: Page,
-  [locations.LOCATION_HOME]: Home,
 };
 
 const App = () => {
-  const sdk = useSDK();
+  const sdk = useSDK<FieldAppSDK>();
 
   const Component = useMemo(() => {
     for (const [location, component] of Object.entries(ComponentLocationSettings)) {
@@ -28,9 +16,10 @@ const App = () => {
         return component;
       }
     }
+    return null;
   }, [sdk.location]);
 
-  return Component ? <Component /> : null;
+  return Component ? <Component sdk={sdk} /> : null;
 };
 
 export default App;
